c9066f17c09eaf8c9f04b0440318928d81e87512
Yurii <erik.mass132@gmail.com>
2022-05-30 22:47:34 +0300

    firs commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// const result = confirm('Are you here?');
+
+// console.log(result);
+
+const vopros = prompt("How old are you ?", "");
+
+console.log(vopros);
---
+++ js/learn.js
@@ -0,0 +1,41 @@
+// // Змінні
+
+// // Змінна її вміст можна змінювати
+// let melika = 5; 
+
+// // Константа не можна змінювати вміст 
+// const test = 5; 
+
+// // null = Значить що цього не існує
+
+// // undefinded = Значить що це існує, але його не знайдено
+// // Приклад: 
+// let priclad; 
+// console.log(priclad);
+
+// // Це обєкт та його свойства
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // Витягування свойства обєкта
+// console.log(obj.name);
+
+// // Массив - в ньому свойства ідуть строго по порядку
+
+// let arr = ['aplle.png', 'orange.jpg', 'orion.bmp'];
+
+// // Витягування свойства обєкта
+// console.log(arr[1]);
+
+// // Вся інформація яка приходить від користувача це строка 'String'
+
+// const result = confirm('Are you here?');
+
+// console.log(result);
+
+// const vopros = prompt("How old are you ?", "");
+
+// console.log(vopros);
