652d61a21ea995ee4f5113d1847b63605f593134
Yurii <erik.mass132@gmail.com>
2022-06-01 22:58:00 +0300

    startCreateMovieDB

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
+"use strict";
 // const result = confirm('Are you here?');
 
 // console.log(result);
 
-const vopros = prompt("How old are you ?", "");
+// const vopros = prompt("How old are you ?", "");
 
-console.log(vopros);
+// console.log(vopros);
+
+
+const numberOffilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Кращий фільм цього року?', ''),
+      b = prompt('На скільки його оціните?', ''),
+      c = prompt('Кращий фільм цього року?', ''),
+      d = prompt('На скільки його оціните?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
