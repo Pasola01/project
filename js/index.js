ad3e19482e3a7edcff7f524f6c457de19cfe87ff
Yurii <erik.mass132@gmail.com>
2022-06-02 22:31:16 +0300

    uslovija

--- js/index.js
+++ js/index.js
@@ -27,3 +27,39 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+// Условия!!!
+
+if (4==9) {
+    console.log('Ok');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Mnoho');
+} else {
+    console.log('Ok');
+}
+
+(num == 50) ? console.log('Ok') : console.log('Error');
+
+const sum = 100;
+
+switch (sum) {
+    case 10:
+        console.log('Bad!');
+        break;
+    case 70:
+        console.log('Ico');
+        break;
+    case 1000:
+        console.log('Ok');
+        break;
+    default:
+        console.log('Choto');
+}
